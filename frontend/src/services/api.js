25c53f1d2f93110b42ea1e8e2adc53c4f26db83d
Mukulsh26 <mukulsh7496@gmail.com>
2026-06-03 00:57:13 +0530

    Fixed URL Issue

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://inventory-management-system-cazo.onrender.com/",
+  baseURL: "https://inventory-management-system-cazo.onrender.com",
 });
+
+
 
 export default api;
